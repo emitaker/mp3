5dae1c5d22f338fe3d6394cccd20ecda423cc7d4
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-06-02 15:53:46 -0500

    Create rotatory_encoder.py

---
+++ tests/rotatory_encoder.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import playlists
+ 
+counter = 10
+
+sound = 10
+playlists.sb_media_player.audio_set_volume(sound)
+
+Enc_A = 18  
+Enc_B = 16  
+ 
+ 
+def init():
+    print("Rotary Encoder Test Program")
+    GPIO.setwarnings(True)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(Enc_A, GPIO.IN)
+    GPIO.setup(Enc_B, GPIO.IN)
+    GPIO.add_event_detect(Enc_A, GPIO.RISING, callback=rotation_decode, bouncetime=10)
+    return
+ 
+ 
+def rotation_decode(Enc_A):
+    global counter
+    global sound
+    sleep(0.002)
+    Switch_A = GPIO.input(Enc_A)
+    Switch_B = GPIO.input(Enc_B)
+ 
+    if (Switch_A == 1) and (Switch_B == 0):
+        counter += 1
+        sound += 1
+        print("direction -> ", counter)
+        while Switch_B == 0:
+            Switch_B = GPIO.input(Enc_B)
+        while Switch_B == 1:
+            Switch_B = GPIO.input(Enc_B)
+        return
+ 
+    elif (Switch_A == 1) and (Switch_B == 1):
+        counter -= 1
+        sound -= 1
+        print("direction <- ", counter)
+        while Switch_A == 1:
+            Switch_A = GPIO.input(Enc_A)
+        return
+    else:
+        return
+ 
+def main():
+    try:
+        init()
+        while True :
+            playlists.sb_media_player.play()
+            sleep(1)
+ 
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+ 
+if __name__ == '__main__':
+    main()
