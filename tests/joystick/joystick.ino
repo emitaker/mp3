e706e15397865610beab8cc974b061ff60b42fe3
emitaker <emiliocamuzanomejia@hotmail.com>
2021-06-06 14:52:30 -0500

    bug fix, right and left now works properly

--- tests/joystick/joystick.ino
+++ tests/joystick/joystick.ino
@@ -1,15 +1,27 @@
-void setup(){Serial.begin(9600);}
+int buttonState = 0;         
+
+void setup()
+{
+  pinMode(7,INPUT_PULLUP);
+  Serial.begin(9600);
+}
 void loop(){
   int y = 0;
   int joystickyaxispin = analogRead(A3); 
   int joystickxaxispin = analogRead(A4);
   int mappedxaxis = map(joystickxaxispin + 18,0,1023,-6,5);
   int mappedyaxis = map(joystickyaxispin + 18,0,1023,-5,5);
-  if(mappedxaxis == -5 || mappedxaxis == -6){
-    Serial.println("right");
+  buttonState = digitalRead(7);
+  
+  if(buttonState == LOW){
+    Serial.println("pushed");
     delay(150);
   }
   else if(mappedxaxis == 5){
+    Serial.println("right");
+    delay(150);
+  }
+  else if(mappedxaxis == -5 || mappedxaxis == -6){
     Serial.println("left");
     delay(150);
   }
@@ -21,8 +33,9 @@ void loop(){
     Serial.println("up");
     delay(150);
   }
-  if(mappedyaxis==5 || mappedyaxis==6){
+  else if(mappedyaxis==5 || mappedyaxis==6){
     Serial.println("down");
     delay(150);
   }
+ 
 }
