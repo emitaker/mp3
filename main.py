5a8e136913026c6ed2591fff5e517920d6ef66b5
emitaker <emiliocamuzanomejia@hotmail.com>
2021-06-06 16:48:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -45,10 +45,80 @@ def slowed_reverb():
     print(playlist)
 
 
+def slowed_reverb_screen():
+    font = pygame.font.SysFont(None, 48)
+    img = font.render('Slowed & reverb', True, ('#f6f6f6')) # #f6f6f6 = white
+    screen.blit(img, (110,65))
+    #Musicicon2
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/slowed&reverb.jpg')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (50,55))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/pausab.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (375,450))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/playb.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (310,451))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/prevsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (62,62))
+    screen.blit(image, (242,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/ffsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (58,58))
+    screen.blit(image, (445,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/shufflew.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (512,456))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/vol.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (188,456))
+
+
 def live():
     global playlist
     playlist = 2
     print(playlist)
+
+
+def live_screen():
+    font = pygame.font.SysFont(None, 48)
+    img = font.render('Live', True, ('#f6f6f6')) # #f6f6f6 = white
+    screen.blit(img, (110,65))
+    #Musicicon2
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/live.jpg')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (50,55))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/pausab.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (375,450))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/playb.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (310,451))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/prevsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (62,62))
+    screen.blit(image, (242,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/ffsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (58,58))
+    screen.blit(image, (445,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/shufflew.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (512,456))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/vol.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (188,456))
 
 
 def classic():
@@ -57,10 +127,80 @@ def classic():
     print(playlist)
 
 
+def classic_screen():
+    font = pygame.font.SysFont(None, 48)
+    img = font.render('Classic', True, ('#f6f6f6')) # #f6f6f6 = white
+    screen.blit(img, (110,65))
+    #Musicicon2
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/classicmusic.jpg')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (50,55))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/pausab.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (375,450))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/playb.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (310,451))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/prevsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (62,62))
+    screen.blit(image, (242,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/ffsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (58,58))
+    screen.blit(image, (445,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/shufflew.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (512,456))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/vol.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (188,456))
+
+
 def rock():
     global playlist
     playlist = 4
     print(playlist)
+
+
+def rock_screen():
+    font = pygame.font.SysFont(None, 48)
+    img = font.render('Rock', True, ('#f6f6f6')) # #f6f6f6 = white
+    screen.blit(img, (110,65))
+    #Musicicon2
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/ock.jpg')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (50,55))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/pausab.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (375,450))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/playb.png')
+    image.convert()
+    image = pygame.transform.scale(image, (65,65))
+    screen.blit(image, (310,451))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/prevsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (62,62))
+    screen.blit(image, (242,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/ffsi.png')
+    image.convert()
+    image = pygame.transform.scale(image, (58,58))
+    screen.blit(image, (445,452))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/shufflew.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (512,456))
+    image = pygame.image.load('/home/pi/Documents/mp3/pics/vol.png')
+    image.convert()
+    image = pygame.transform.scale(image, (50,50))
+    screen.blit(image, (188,456))
 
 def play():
     '''Play the music when the button is pushed'''
@@ -168,24 +308,184 @@ def rotation_decode(Enc_A):
 def main():
     try:
         init()
+
+        #-------------------------------------------------------------------------
+        #
+        #Weather Menu
+        #
+        #-------------------------------------------------------------------------
+
+        weather_theme = pygame_menu.themes.THEME_DARK.copy()
+        weather_theme.title_background_color=('#fc3a4b') # This line puts the title background in some kind of red color
+        weather_theme.title_bar_style =  pygame_menu.widgets.MENUBAR_STYLE_SIMPLE
+        weather_theme.title_offset = (20,0)
+        weather_theme.background_color=('#111010') # Black
+
+
+        weather_menu = pygame_menu.Menu(
+            height=650, #height
+            theme=weather_theme,
+            title='Weather',
+            width=800,
+            joystick_enabled=True
+        )
+
+        weather_menu.add.button('Back',pygame_menu.events.BACK)
+        #-------------------------------------------------------------------------
+        #
+        #Playlists Menu
+        #
+        #-------------------------------------------------------------------------
+
+        playlists_theme = pygame_menu.themes.THEME_DARK.copy()
+        playlists_theme.title_background_color=('#fc3a4b') # This line puts the title background in some kind of red color
+        playlists_theme.title_bar_style =  pygame_menu.widgets.MENUBAR_STYLE_SIMPLE
+        playlists_theme.title_offset = (20,0)
+        playlists_theme.background_color=('#111010') # Black
+        playlists_theme.widget_font_size = 45
+
+        playlists_menu = pygame_menu.Menu(
+            height=650, #height
+            theme=playlists_theme,
+            title='Playlists',
+            width=800,
+            joystick_enabled=True
+        )
+
+        #playlists_menu.add_image('D:\Programacion\Programs\mp3\pics\slow.jpg', scale=(0.25,0.25),margin=(-370,15))
+        #playlists_menu.add_image('D:\Programacion\Programs\mp3\pics\live.jpg', scale=(0.25,0.25),margin=(-370,15))
+        #playlists_menu.add_image('D:\Programacion\Programs\mp3\pics\classicmusic.jpg', scale=(0.09,0.09),margin=(-370,15))
+        #playlists_menu.add_image('D:\Programacion\Programs\mp3\pics\ock.jpg', scale=(0.25,0.25),margin=(-370,-305))
+
+        playlists_menu.add_image('/home/pi/Documents/mp3/pics/slow.jpg', scale=(0.25,0.25),margin=(-370,15))
+        playlists_menu.add_image('/home/pi/Documents/mp3/pics/live.jpg', scale=(0.25,0.25),margin=(-370,15))
+        playlists_menu.add_image('/home/pi/Documents/mp3/pics/classicmusic.jpg', scale=(0.09,0.09),margin=(-370,15))
+        playlists_menu.add_image('/home/pi/Documents/mp3/pics/ock.jpg', scale=(0.25,0.25),margin=(-370,-305))
+
+
+        playlists_menu.add.button('Slowed & Reverb', slowed_reverb, margin=(-140,12))
+        playlists_menu.add.button('Live', live, margin=(-280,10))
+        playlists_menu.add.button('Classic',classic, margin=(-252,10))
+        playlists_menu.add.button('Rock',rock, margin=(-269,10))
+        playlists_menu.add.button('Back',pygame_menu.events.BACK, margin=(-269,0))
+        playlists_menu.add_vertical_margin(230)
+
+
+        #-------------------------------------------------------------------------
+        #
+        #Main Menu
+        #
+        #-------------------------------------------------------------------------
+
+        main_menu_theme = pygame_menu.themes.THEME_DARK.copy()
+        main_menu_theme.title_bar_style =  pygame_menu.widgets.MENUBAR_STYLE_NONE
+        main_menu_theme.background_color=('#111010') # Black
+        main_menu_theme.widget_font_size = 55
+
+        menu = pygame_menu.Menu(
+            height=650, #height
+            theme=main_menu_theme,
+            title='',
+            width=800,
+        )
+
+        #menu.add_image('D:\Programacion\Programs\mp3\pics\musicicon2.png', scale=(.2,.2),margin=(-170,-211))
+        #menu.add_image('D:\Programacion\Programs\mp3\pics\weather.png', scale=(.2,.2),margin=(170,20))
+
+        menu.add_image('/home/pi/Documents/mp3/pics/musicicon2.png', scale=(.2,.2),margin=(-170,-211))
+        menu.add_image('/home/pi/Documents/mp3/pics/weather.png', scale=(.2,.2),margin=(170,20))
+
+        menu.add.button('Music', playlists_menu, margin=(-167,-85))
+        menu.add.button('Weather', weather_menu,margin=(174,0))
+        menu.add_vertical_margin(120)
+
+
         while True :
             state_pb_play = GPIO.input(pb_play)
             state_pb_pause = GPIO.input(pb_pause)
             state_pb_skip = GPIO.input(pb_skip)
             state_pb_prev = GPIO.input(pb_prev)
             state_pb_shuffle = GPIO.input(pb_shuffle)
+            ser_bytes = ser.readline()#--------------------------------------------------------------------------------------------------
+            decoded_bytes = ser_bytes[0:len(ser_bytes)-2].decode("utf-8")#--------------------------------------------------------------------------------------------------------------
 
-            if state_pb_play == True:
-                play()
-            if state_pb_pause == True:
-                pause()
-            if state_pb_skip == True:
-                skip()
-            if state_pb_prev == True:
-                prev()
-            if state_pb_shuffle == True:
-                shuffle()
-            time.sleep(1)
+            current_menu = menu.get_current()
 
+            if current_menu.get_title() != 'MENU' or not menu.is_enabled():
+                if playlist = 1:
+                    slowed_reverb_screen()
+                    if state_pb_play == True:
+                        play()
+                    if state_pb_pause == True:
+                        pause()
+                    if state_pb_skip == True:
+                        skip()
+                    if state_pb_prev == True:
+                        prev()
+                    if state_pb_shuffle == True:
+                        shuffle()
+                    time.sleep(1)
+                elif playlist = 2:
+                    live_screen()
+                    if state_pb_play == True:
+                        play()
+                    if state_pb_pause == True:
+                        pause()
+                    if state_pb_skip == True:
+                        skip()
+                    if state_pb_prev == True:
+                        prev()
+                    if state_pb_shuffle == True:
+                        shuffle()
+                    time.sleep(1)
+                elif playlist = 3:
+                    classic_screen()
+                    if state_pb_play == True:
+                        play()
+                    if state_pb_pause == True:
+                        pause()
+                    if state_pb_skip == True:
+                        skip()
+                    if state_pb_prev == True:
+                        prev()
+                    if state_pb_shuffle == True:
+                        shuffle()
+                    time.sleep(1)
+                elif playlist = 4:
+                    rock_screen()
+                    if state_pb_play == True:
+                        play()
+                    if state_pb_pause == True:
+                        pause()
+                    if state_pb_skip == True:
+                        skip()
+                    if state_pb_prev == True:
+                        prev()
+                    if state_pb_shuffle == True:
+                        shuffle()
+                    time.sleep(1)
+                else:
+                    screen.fill('#111010')
+
+                if decoded_bytes == "pushed":
+                    pushed_event=pygame.event.Event(pygame.USEREVENT, attr1='pushed_event')
+                    pygame.event.post(pushed_event)
+                    print(pushed_event.type)
+
+                events = pygame.event.get()
+
+                for event in events:
+                    print(event)
+                    if event.type == pygame.QUIT:
+                        exit()
+                    elif event.type == 32847:
+                        print('pushed_event')
+                        menu.toggle()
+
+                if menu.is_enabled():
+                    menu.draw(screen)
+                    menu.update(events)
+
+                pygame.display.flip()
     except KeyboardInterrupt:
         GPIO.cleanup()
