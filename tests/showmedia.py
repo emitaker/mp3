6d8ff245fbe723398215e6e243dc1d9e99fdc04a
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-06-07 14:01:03 -0500

    Update showmedia.py

--- tests/showmedia.py
+++ tests/showmedia.py
@@ -2,7 +2,7 @@ import playlists
 import vlc
 import time
 
-playlists.slowed_reverb_player.play()
+playlists.slowed_reverb_player.play_item_at_index(3)
 value = playlists.slowed_reverb_player.is_playing()
 print(value)
 time.sleep(20)
