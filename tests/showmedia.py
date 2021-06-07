57732e39cf5ad5088914f9f720743078e82d16da
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-06-07 14:44:20 -0500

    Update showmedia.py

--- tests/showmedia.py
+++ tests/showmedia.py
@@ -6,7 +6,6 @@ playlists.slowed_reverb_player.play_item_at_index(4)
 x = playlists.slowed_reverb_player
 value = playlists.slowed_reverb_player.is_playing()
 if value:
-    for song in x:
-        song
+    print(x[1])
 time.sleep(5)
 
