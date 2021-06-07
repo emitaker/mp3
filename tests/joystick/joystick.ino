cd2799b2bdbfb7e19faad6d68808e54606f4a89e
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-06-07 10:31:07 -0500

    Update joystick.ino

--- tests/joystick/joystick.ino
+++ tests/joystick/joystick.ino
@@ -7,35 +7,35 @@ void setup()
 }
 void loop(){
   int y = 0;
-  int joystickyaxispin = analogRead(A3); 
-  int joystickxaxispin = analogRead(A4);
+  int joystickyaxispin = analogRead(A8); 
+  int joystickxaxispin = analogRead(A9);
   int mappedxaxis = map(joystickxaxispin + 18,0,1023,-6,5);
   int mappedyaxis = map(joystickyaxispin + 18,0,1023,-5,5);
-  buttonState = digitalRead(7);
+  buttonState = digitalRead(0);
   
   if(buttonState == LOW){
     Serial.println("pushed");
-    delay(150);
+    delay(40);
   }
   else if(mappedxaxis == 5){
     Serial.println("right");
-    delay(150);
+    delay(2);
   }
   else if(mappedxaxis == -5 || mappedxaxis == -6){
     Serial.println("left");
-    delay(150);
+    delay(2);
   }
   else if(mappedyaxis == -1 || mappedyaxis == 0 || mappedyaxis == -2 || mappedyaxis == -4){
     Serial.println("nothing");
-    delay(150);
+    delay(2);
   }
   else if(mappedyaxis == -5){
     Serial.println("up");
-    delay(150);
+    delay(2);
   }
   else if(mappedyaxis==5 || mappedyaxis==6){
     Serial.println("down");
-    delay(150);
+    delay(2);
   }
  
 }
