633267df5a3cd3b937e9d003a796d16995de2b4b
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-06-08 22:27:32 -0500

    Update joystick.ino

--- tests/joystick/joystick.ino
+++ tests/joystick/joystick.ino
@@ -1,17 +1,31 @@
-int buttonState = 0;         
+#include "DHT.h" // esto es para el sensor de temperatura
+#define DHTTYPE DHT11   
+const int DHTPin = 44;     
+DHT dht(DHTPin, DHTTYPE);
 
+int buttonState = 0;      
+   
 void setup()
 {
-  pinMode(7,INPUT_PULLUP);
+  pinMode(30,INPUT_PULLUP);
   Serial.begin(9600);
+  dht.begin();
 }
 void loop(){
   int y = 0;
   int joystickyaxispin = analogRead(A8); 
   int joystickxaxispin = analogRead(A9);
+
+  //sensor de temperatura
+  float t = dht.readTemperature();
+  
   int mappedxaxis = map(joystickxaxispin + 18,0,1023,-6,5);
   int mappedyaxis = map(joystickyaxispin + 18,0,1023,-5,5);
-  buttonState = digitalRead(0);
+  buttonState = digitalRead(30);
+
+  Serial.println("Temp: ");
+  Serial.println(t);
+  delay(250);
   
   if(buttonState == LOW){
     Serial.println("pushed");
