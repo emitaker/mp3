3d06f2f84ebfb2de3540059a412b712e2655afbb
Emilio Campuzano Mejía <a01378948@itesm.mx>
2021-06-09 21:58:28 -0500

    Final commit, comments

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ import vlc
 import time
 import RPi.GPIO as GPIO
 import random
-#import MediaInfo #to do
 import playlists
 import pygame
 import pygame_menu
 import serial
 
-pygame.init()
+pygame.init() #initializing pygame library
 
 ser = serial.Serial('/dev/ttyACM0',9600)#serial port
-ser.flushInput()
+ser.flushInput() 
 
 pb_play = 17 #pin 11
 pb_pause = 27 #pin 13
 pb_skip = 22 #pin 15
 pb_prev = 5 #pin 29
 pb_shuffle = 6 #pin 31
-Enc_A = 18
-Enc_B = 16
+Enc_A = 18 #pin 12
+Enc_B = 16 #pin 36
 
-playlist = 1
+playlist = 1 #default playlist
 
-sound = 60
+sound = 60 #default volume
 
-screen = pygame.display.set_mode((800, 650))
+screen = pygame.display.set_mode((800, 650))#screen resolution: 800 width x 650 height
 
 def init():
+    """Defining all the GPIO ports"""
     GPIO.setwarnings(True)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pb_play, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -42,12 +42,14 @@ def init():
 
 
 def slowed_reverb():
+    """Defines slowed & reverb as current playlist"""
     global playlist
     playlist = 1
     print(playlist)
 
 
 def slowed_reverb_screen():
+    """Drawing the slowed & reverb playlist screen"""
     screen.fill(('#111010'))
     font = pygame.font.SysFont(None, 48)
     img = font.render('Slowed & reverb', True, ('#f6f6f6')) # #f6f6f6 = white
@@ -92,12 +94,14 @@ def slowed_reverb_screen():
 
 
 def live():
+    """Defines live music as current playlist"""
     global playlist
     playlist = 2
     print(playlist)
 
 
 def live_screen():
+    """Drawing the live music playlist screen"""
     screen.fill(('#111010'))
     font = pygame.font.SysFont(None, 48)
     img = font.render('Live', True, ('#f6f6f6')) # #f6f6f6 = white
@@ -142,12 +146,14 @@ def live_screen():
 
 
 def classic():
+    """Defines classic music as current playlist"""
     global playlist
     playlist = 3
     print(playlist)
 
 
 def classic_screen():
+    """Drawing the classic music playlist screen"""
     screen.fill(('#111010'))
     font = pygame.font.SysFont(None, 48)
     img = font.render('Classic', True, ('#f6f6f6')) # #f6f6f6 = white
@@ -192,12 +198,14 @@ def classic_screen():
 
 
 def rock():
+    """Defines rock music as current playlist"""
     global playlist
     playlist = 4
     print(playlist)
 
 
 def rock_screen():
+    """Drawing the rock music playlist screen"""
     screen.fill(('#111010'))
     font = pygame.font.SysFont(None, 48)
     img = font.render('Rock', True, ('#f6f6f6')) # #f6f6f6 = white
@@ -243,7 +251,7 @@ def rock_screen():
 
 
 def play():
-    '''Play the music when the button is pushed'''
+    """Plays the first song of the playlist when the button is pushed"""
     if (playlist == 1):
         playlists.slowed_reverb_player.play()
     elif(playlist == 2):
@@ -255,7 +263,7 @@ def play():
 
 
 def pause():
-    '''Pause the music when the button is pushed'''
+    """Pause the music when the button is pushed"""
     if (playlist == 1):
         playlists.slowed_reverb_player.pause()
     elif(playlist == 2):
@@ -267,7 +275,7 @@ def pause():
 
 
 def skip():
-    '''Pass to the next '''
+    """Pass to the next song of the playlist"""
     if (playlist == 1):
         playlists.slowed_reverb_player.next()
     elif(playlist == 2):
@@ -279,7 +287,7 @@ def skip():
 
 
 def prev():
-    '''Pass to the prev song'''
+    """Return to the previous song of the playlist"""
     if (playlist == 1):
         playlists.slowed_reverb_player.previous()
     elif(playlist == 2):
@@ -291,7 +299,7 @@ def prev():
 
 
 def shuffle():
-    '''Plays a random song in the playlist'''
+    """Plays a random song of the current playlist"""
     index = random.randint(0,4)
     print(index)
 
@@ -306,6 +314,7 @@ def shuffle():
 
 
 def rotation_decode(Enc_A):
+    """Set the volume up/down"""
     global sound
     time.sleep(0.002)
     Switch_A = GPIO.input(Enc_A)
@@ -571,5 +580,6 @@ def main():
             menu.update(events)
 
         pygame.display.flip()
+
 
 main()
